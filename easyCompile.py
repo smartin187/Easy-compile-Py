5fda070e70ac1a2577342ccadf1924038b9ffee8
sunti <sunti.martin@gmail.com>
2026-01-23 13:12:30 +0100

    création de l'interface

--- easyCompile.py
+++ easyCompile.py
@@ -6,9 +6,41 @@ This module have the fonction "easyCompile", for easy compile Python.
 
 import tkinter as tk
 from tkinter import ttk
+
 import sys
 
 os_name = sys.platform
+
+class Trad:
+    """The traduction for easyCompile.
+    Example : {"fr":"aa", "en":"aa"}
+    """
+
+    t001 = {
+        "fr":"Choisiser le type de compilation pour Window :",
+        "en":"Select the type of compiling for Window:"
+    }
+
+    t002 = {
+        "fr":"Type de compilation",
+        "en":"Compiling type"
+    }
+
+    t003 = {
+        "fr":"Configuration",
+        "en":"Configure"
+    }
+
+    t004 = {
+        "fr":"Aucun paramètre...",
+        "en":"No configure..."
+    }
+
+    t005 = {
+        "fr":"Compiler",
+        "en":"Compile"
+    }
+
 
 def easyCompile(window=None, file=None, language="en", title="Easy compile Py"):
     """Open an window for compile Python with GUI.
@@ -17,6 +49,35 @@ def easyCompile(window=None, file=None, language="en", title="Easy compile Py"):
     * file : the file of scrip python
     * title : the title of windows. Defaut is "Easy compile Py".
     """
+    def configure_frame_windows(frame):
+        """Make the frame of the compile for window."""
+        frame_type_compiling = tk.LabelFrame(frame, text=Trad.t002[language])
+
+        text_info_compiling = tk.Label(frame_type_compiling, text=Trad.t001[language])
+        text_info_compiling.pack()
+
+        text_compile = tk.StringVar()
+        text_compile.set("Windows application | *.exe")
+
+        list_compiling = ttk.Combobox(frame_type_compiling, values=["Windows application | *.exe"], state="readonly", textvariable=text_compile, width=25)     # meilleur texte + mettre par défaut le texte
+        list_compiling.pack()
+
+        frame_type_compiling.pack()
+
+        frame_config_compile = tk.LabelFrame(frame, text=Trad.t003[language])
+
+        texte_config = tk.Label(frame_config_compile, text=Trad.t004[language])
+        texte_config.pack()
+
+        frame_config_compile.pack()
+
+        frame_compile = tk.LabelFrame(frame, text=Trad.t005[language])
+
+        button_compile = tk.Button(frame_compile, text=Trad.t005[language], font=("Arial", 25))
+        button_compile.pack()
+
+        frame_compile.pack()
+
 
     if window is None:
         raise Exception("No window was given.")
@@ -35,11 +96,14 @@ def easyCompile(window=None, file=None, language="en", title="Easy compile Py"):
     # Window :
     frame_compile_Window = tk.Frame(compile_notebok)
     compile_notebok.add(frame_compile_Window, text="Window", state="normal" if os_name == "win32" else "disabled")
+
+    configure_frame_windows(frame_compile_Window)
     
     # Linux :
     frame_compile_Linux = tk.Frame(compile_notebok)
     compile_notebok.add(frame_compile_Linux, text="Linux", state="normal" if os_name == "linux" else "disabled")
     
+    window_easy_compile.geometry("300x300")
 
     window_easy_compile.grab_set()
     window_easy_compile.wait_window()
